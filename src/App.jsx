91d34687b78bccf0ede6606739bf8853a125edd1
HanMinSS <gksals6464@naver.com>
2022-01-16 12:31:50 +0900

    🚚  init router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Splash from "./components/splash/splash";
 
-function App() {
-  return <div>감귤 마켓</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/login" element={<div>login false</div>} />
+        <Route path="/feed" element={<div>login true</div>} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/components/splash/splash.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Splash = () => (
+  <div className="logo_wrap">
+    <img
+      className="img_logo"
+      src="/images/splash/full-logo.png"
+      alt="로고 이미지"
+    />
+  </div>
+);
+
+export default Splash;
