13d79063cd2f1f88f0e2b6671314ae2bb7f2c59e
HanMinSS <gksals6464@naver.com>
2022-01-17 14:16:43 +0900

    💄  로그인 페이지 스타일 적용

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,26 +1,39 @@
 import React from "react";
+import styles from "./login.module.css";
 
 const Login = () => {
   return (
-    <main className="main">
-      <h1 className="title">로그인</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}>로그인</h1>
       <form>
-        <div className="input_wrap">
-          <label htmlFor="email">이메일</label>
-          <input id="email" type="email" name="eMail" />
-          <p id="emailError" className="err_msg"></p>
+        <div className={styles.input_wrap}>
+          <label className={styles.label} htmlFor="email">
+            이메일
+          </label>
+          <input className={styles.input} id="email" type="email" />
+          <p className={styles.err_msg}></p>
         </div>
 
-        <div className="input_wrap">
-          <label htmlFor="pwd">비밀번호</label>
-          <input id="pwd" type="password" name="pwd" />
+        <div className={styles.input_wrap}>
+          <label className={styles.label} htmlFor="pwd">
+            비밀번호
+          </label>
+          <input className={styles.input} id="pwd" type="password" />
         </div>
 
-        <button id="submitBtn" className="btn_submit" type="button" disabled>
+        <button
+          id="submitBtn"
+          className={styles.btn_submit}
+          type="button"
+          disabled
+        >
           로그인
         </button>
       </form>
-      <a className="link_register" href="/templates/join_membership.html">
+      <a
+        className={styles.link_register}
+        href="/templates/join_membership.html"
+      >
         이메일로 회원가입
       </a>
     </main>
