3b5de2dac340268b5242ee98af6219dc18700222
HanMinSS <gksals6464@naver.com>
2022-01-18 17:24:11 +0900

    ✅  other chat(말풍선) 마크업

--- src/components/chat/chatRoom/chatRoom.jsx
+++ src/components/chat/chatRoom/chatRoom.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ChatRoomHeader from "./chatRoomHeader";
 import InputBar from "./inputBar";
+import styles from "./chatRoom.module.css";
+import OtherChat from "./otherChat";
 
 const ChatRoom = () => {
   return (
     <>
       <ChatRoomHeader />
       <InputBar />
+      <main className={styles.main}>
+        <OtherChat />
+      </main>
     </>
   );
 };
