88db26c6f0dd3c4af5e4ca00ca0a24cbfc1db322
HanMinSS <gksals6464@naver.com>
2022-01-18 17:28:12 +0900

    💄  Other chat style

--- src/components/chat/chatRoom/otherChat.jsx
+++ src/components/chat/chatRoom/otherChat.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import styles from "./otherChat.module.css";
 
 const OtherChat = () => {
   return (
-    <article className="other_chat">
+    <article className={styles.other_chat}>
       <img
-        className="profile_img"
-        src="../src/images/chat/basic-profile-img.png"
+        className={styles.profile_img}
+        src="/images/publicImg/basic-profile-img.png"
         alt=""
       />
-      <div className="text_wrap">
-        <div className="bubble other_bubble">
+      <div className={styles.text_wrap}>
+        <div className={styles.bubble}>
           옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다.
           이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게
           뛰노는 인생의 힘있다.
         </div>
-        <p className="text_time">12:39</p>
+        <p className={styles.text_time}>12:39</p>
       </div>
     </article>
   );
