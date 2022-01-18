37346d2d7b0498557f3b500561b3779b1ade8a41
HanMinSS <gksals6464@naver.com>
2022-01-18 17:35:01 +0900

    💄  myChat style

--- src/components/chat/chatRoom/myChat.jsx
+++ src/components/chat/chatRoom/myChat.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./myChat.module.css";
 
 const MyChat = () => {
   return (
-    <article className="my_chat">
-      <p className="text_time">12:50</p>
-      <div className="bubble my_bubble">네 말씀하세요.</div>
+    <article className={styles.my_chat}>
+      <p className={styles.text_time}>12:50</p>
+      <div className={styles.bubble}>네 말씀하세요.</div>
     </article>
   );
 };
