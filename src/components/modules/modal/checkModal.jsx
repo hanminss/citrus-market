49ada049d95fb18ab219f44f764dfbc1aaba93cc
HanMinSS <gksals6464@naver.com>
2022-01-22 18:51:58 +0900

    ✅  check modal on/off 구현

--- src/components/modules/modal/checkModal.jsx
+++ src/components/modules/modal/checkModal.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./checkModal.module.css";
-const CheckModal = () => (
+const CheckModal = ({ handleCheckModal }) => (
   <section className={styles.outer}>
     <div className={styles.checkModal}>
       <p className={styles.modal_msg}>게시글을 삭제할까요?</p>
       <div>
-        <button>취소</button>
+        <button onClick={handleCheckModal}>취소</button>
         <button>삭제</button>
       </div>
     </div>
--- src/components/modules/modal/postModal.jsx
+++ src/components/modules/modal/postModal.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import CheckModal from "./checkModal";
 import styles from "./postModal.module.css";
 const PostModal = ({ handleModal }) => {
+  const [checkModal, setCheckModal] = useState(false);
+
+  const handleCheckModal = () => {
+    if (checkModal) setCheckModal(false);
+    else setCheckModal(true);
+  };
   return (
     <>
       <section className={styles.model_wrap}>
@@ -9,9 +15,11 @@ const PostModal = ({ handleModal }) => {
         <div className={styles.modal}>
           <div className={styles.modal_bar} />
           <button className={styles.modalBtn}>수정</button>
-          <button className={styles.modalBtn}>삭제</button>
+          <button className={styles.modalBtn} onClick={handleCheckModal}>
+            삭제
+          </button>
         </div>
-        <CheckModal />
+        {checkModal ? <CheckModal handleCheckModal={handleCheckModal} /> : ""}
       </section>
     </>
   );
