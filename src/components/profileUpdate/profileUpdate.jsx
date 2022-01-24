a2333ef2772c4bb47a752b0527cb81907e540561
HanMinSS <gksals6464@naver.com>
2022-01-24 16:45:05 +0900

    💄  profileUpdate main 마크업, 스타일

--- src/components/profileUpdate/profileUpdate.jsx
+++ src/components/profileUpdate/profileUpdate.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getCookie } from "../../util/cookie";
+import { getMyInfo } from "../../util/fetcher";
 import ProfileHeader from "./profileHeader";
+import ProfileUpdateMain from "./profileUpdateMain";
 
 const ProfileUpdate = () => {
+  const [userInfo, setUserInfo] = useState();
+  const token = getCookie("pic_token");
+  const accountname = getCookie("pic_accountname");
+
+  useEffect(() => {
+    getMyInfo(accountname, token) //
+      .then((res) => {
+        setUserInfo(res.data.profile);
+      });
+  }, []);
   return (
     <>
       <ProfileHeader />
+      {userInfo ? <ProfileUpdateMain userInfo={userInfo} /> : "Loading..."}
     </>
   );
 };
---
+++ src/components/profileUpdate/profileUpdateMain.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./profileUpdateMain.module.css";
+const ProfileUpdateMain = ({ userInfo }) => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.relative_wrap}>
+        <div className={styles.border_wrap}>
+          <img
+            className={styles.img_profile}
+            src={`http://146.56.183.55:5050/${userInfo.image}`}
+            alt="profile-img"
+            accept="image/*"
+          />
+        </div>
+
+        <label htmlFor="img_profile">
+          <img
+            className={styles.img_upload_btn}
+            src="/images/publicImg/upload-file.png"
+            alt="profile-img"
+          />
+        </label>
+        <input type="file" id="img_profile" hidden accept="image/*" />
+      </div>
+
+      <div className={styles.input_wrap}>
+        <label htmlFor="userName">사용자 이름</label>
+        <input
+          type="text"
+          id="userName"
+          name="userName"
+          placeholder="2~10자 이내여야 합니다."
+        />
+        <p className={styles.err_msg}></p>
+      </div>
+
+      <div className={styles.input_wrap}>
+        <label htmlFor="userID">계정 ID</label>
+        <input
+          type="text"
+          id="userID"
+          name="userID"
+          placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+        />
+        <p className={styles.err_msg}></p>
+      </div>
+
+      <div className={styles.input_wrap}>
+        <label htmlFor="userDesc">소개</label>
+        <input
+          type="text"
+          id="userDesc"
+          name="userDesc"
+          placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+          accept="image/*"
+        />
+        <p className={styles.err_msg}></p>
+      </div>
+    </main>
+  );
+};
+
+export default ProfileUpdateMain;
