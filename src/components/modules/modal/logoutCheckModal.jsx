92b32023ac62e623f530bcf627e3aaf8a8e67101
HanMinSS <gksals6464@naver.com>
2022-01-29 22:48:17 +0900

    ✅  로그아웃 기능 구현

--- src/components/modules/modal/logoutCheckModal.jsx
+++ src/components/modules/modal/logoutCheckModal.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { removeCookie } from "../../../util/cookie";
 import styles from "./logoutCheckModal.module.css";
 
 const LogoutCheckModal = ({ setCheckModal }) => {
+  const logout = () => {
+    removeCookie("pic_token");
+    removeCookie("pic_accountname");
+    removeCookie("pic_profile");
+    window.location.replace("/");
+  };
   return (
     <section className={styles.outer}>
       <div className={styles.checkModal}>
         <p className={styles.modal_msg}>로그아웃하시겠어요?</p>
         <div>
           <button onClick={() => setCheckModal(false)}>취소</button>
-          <button>로그아웃</button>
+          <button onClick={logout}>로그아웃</button>
         </div>
       </div>
     </section>
