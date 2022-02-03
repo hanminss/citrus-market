c6b1e4018b03e8101eb15b790071c5cad9a7dcd2
HanMinSS <gksals6464@naver.com>
2022-02-03 22:23:08 +0900

    ✅  404 페이지 구현

--- src/components/404/notFound.jsx
+++ src/components/404/notFound.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import styles from "./notFound.module.css";
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <main>
-      <img src="/images/404/icon-404.png" alt="" />
-      <p>페이지를 찾을 수 없습니다.</p>
-      <button>이전 페이지</button>
+      <section className={styles.contentWrap}>
+        <img src="/images/404/icon-404.png" alt="" />
+        <p className={styles.desc}>페이지를 찾을 수 없습니다.</p>
+        <button className={styles.backBtn} onClick={() => navigate(-1)}>
+          이전 페이지
+        </button>
+      </section>
     </main>
   );
 };
